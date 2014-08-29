40cf5fcb0b46c42e11107d1cbdb62ab82ac0f07c
topos <slacket@gmail.com>
2014-08-29 01:22:18 -0400

    tweaks

---
+++ personal/theme.el
@@ -0,0 +1,7 @@
+(load-theme 'zenburn t)
+(set-background-color "black")
+(set-face-background 'hl-line "#050555")
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(global-linum-mode 1)
